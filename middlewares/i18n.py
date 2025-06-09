65a8803228e38b06b6fc4e8bdc53dc326fde45af
Akbar-dev-uz <akbaryakubov12@gmail.com>
2025-06-09 23:39:24 +0500

    initial i18n

--- middlewares/i18n.py
+++ middlewares/i18n.py
@@ -1,10 +1,14 @@
+from aiogram_i18n.cores.gnu_text_core import GNUTextCore
 from aiogram_i18n import I18nMiddleware
-from routers.functions.funcs import get_locale
+from routers.functions.funcs import get_locale  # если ты определяешь язык сам
 
+core = GNUTextCore(
+    path="locales/{locale}/LC_MESSAGES",  # путь до твоих .mo/.po
+    default_locale="en",
+    domain="messages"
+)
 
 i18n = I18nMiddleware(
-    path="locales",
-    default_locale="en",
-    domain="messages",
-    get_locale=get_locale,
+    core=core,
 )
+i18n.manager.locale_getter = get_locale  # если нужен кастомный выбор языка
